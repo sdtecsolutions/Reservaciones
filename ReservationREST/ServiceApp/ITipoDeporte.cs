2f20acc6f902be0d875aa42e6b2f8661eb991c9c
sdtecsolutions <sdtecsolutions@gmail.com>
2016-12-04 03:49:56 -0500

    Update ITipoDeporte.cs

--- ReservationREST/ServiceApp/ITipoDeporte.cs
+++ ReservationREST/ServiceApp/ITipoDeporte.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ServiceModel;
 using ReservationREST.BusinessEntities;
 using System.ServiceModel.Web;
@@ -12,36 +12,50 @@ namespace ReservationREST.ServiceApp
         /// Lista los tipos de deporte
         /// </summary>
         [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "TipoDeporte", ResponseFormat = WebMessageFormat.Json)]
-        List<BETipoDeporte> getTipoDeportes();
-
-        /// <summary>
-        /// Registrar los tipos de deporte
-        /// </summary>
-        [OperationContract]
-        [WebInvoke(Method = "POST", UriTemplate = "TipoDeporte", ResponseFormat = WebMessageFormat.Json)]
-        BETipoDeporte postTipoDeportes(BETipoDeporte obj);
-
-        /// <summary>
-        /// Actualizar los tipos de deporte
-        /// </summary>
-        [OperationContract]
-        [WebInvoke(Method = "PUT", UriTemplate = "TipoDeporte", ResponseFormat = WebMessageFormat.Json)]
-        BETipoDeporte putTipoDeportes(BETipoDeporte obj);
+        [WebInvoke(Method = "GET", 
+            UriTemplate = "tipodeportes",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        List<BETipoDeporte> ListarTipoDeporte();
 
         /// <summary>
         /// Obtener tipo de deporte
         /// </summary>
         [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "TipoDeporte/{COD_TIPO_DEPO}", ResponseFormat = WebMessageFormat.Json)]
-        List<BETipoDeporte> getTipoDeportes(string COD_TIPO_DEPO);
+        [WebInvoke(Method = "GET",
+            UriTemplate = "tipodeportes/{COD_TIPO_DEPO}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        BETipoDeporte ObtenerTipoDeporte(string COD_TIPO_DEPO);
+
+        /// <summary>
+        /// Registrar los tipos de deporte
+        /// </summary>
+        [OperationContract]
+        [WebInvoke(Method = "POST", 
+            UriTemplate = "tipodeportes",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        BETipoDeporte RegistrarTipoDeporte(BETipoDeporte obj);
+
+        /// <summary>
+        /// Actualizar los tipos de deporte
+        /// </summary>
+        [OperationContract]
+        [WebInvoke(Method = "PUT", 
+            UriTemplate = "tipodeportes",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        BETipoDeporte ActualizarTipoDeporte(BETipoDeporte obj);
 
         /// <summary>
         /// Eliminar los tipos de deporte
         /// </summary>
         [OperationContract]
-        [WebInvoke(Method = "DELETE", UriTemplate = "TipoDeporte/{COD_TIPO_DEPO}", ResponseFormat = WebMessageFormat.Json)]
-        void deleteTipoDeportes(string COD_TIPO_DEPO);
-
+        [WebInvoke(Method = "DELETE", 
+            UriTemplate = "tipodeportes/{COD_TIPO_DEPO}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        BETipoDeporte EliminarTipoDeporte(string COD_TIPO_DEPO);
     }
 }
